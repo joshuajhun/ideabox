aaff857b4e3242634b5c6cf16950cfeb4864bc55
joshuajhun <joshuajhun@gmail.com>
2016-02-01 16:06:57 -0700

    created a dashboard controller and index action and set the root of the application to be the index of the dash

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
+
   namespace :api do
     namespace :v1 do
       resources :ideas, only: [:index, :create, :show, :destroy, :update], defaults: {format: :json} do
+      end
     end
   end
-end 
 end
