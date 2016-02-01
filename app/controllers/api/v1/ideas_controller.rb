3b627d8cf543722e0916e686e19768b6d05ed932
joshuajhun <joshuajhun@gmail.com>
2016-02-01 16:50:29 -0700

    api is built out. I can test the index and show by going to the url but for the other functionality I have to test with Factory Girl and see if it is also coming through with ajax

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,4 +4,25 @@ class Api::V1::IdeasController < ApplicationController
   def index
     respond_with Idea.all
   end
+
+  def show
+    respond_with Idea.find_by(id: params[:id])
+  end
+
+  def create
+    respond_with Idea.create(idea_params)
+  end
+
+  def update
+    respond_with Item.update(params[:id], item_params)
+  end
+
+  def destroy
+    respond_with Item.destroy(params[:id])
+  end
+
+  private
+  def idea_params
+    params.require(:idea).permit(:id, :title, :body, :quantity)
+  end
 end
