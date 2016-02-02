60dab40b88707555ad11decbe2a2948f606f3104
joshuajhun <joshuajhun@gmail.com>
2016-02-02 11:31:53 -0700

    moved posting function out of the dom and into a seperate service file.

--- app/assets/javascripts/ideabox_service.js
+++ app/assets/javascripts/ideabox_service.js
@@ -7,3 +7,21 @@ function fetchIndex(){
     }
   })
 }
+
+function postIdea(){
+  $('#create-idea').on('click', function(){
+
+    var ideaParams = { name: $('#idea-name').val(), body: $("#idea-body").val()}
+    $.ajax({
+      type: 'POST',
+      url: 'http://socalbros:3000/api/v1/ideas',
+      data: ideaParams,
+      success: function(newIdea){
+        renderIndex(newIdea)
+      },
+      error: function(xhr){
+        console.log(xhr.responseText)
+      }
+    })
+  })
+}
--- app/assets/javascripts/render_ideas.js
+++ app/assets/javascripts/render_ideas.js
@@ -1,3 +1,4 @@
 function renderIdeas(){
   fetchIndex()
+  postIdea()
 }
